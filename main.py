bdddf469096793e6372cd974b31e254c7e024ed2
Kevin Santiago <113930456+ksantiago772@users.noreply.github.com>
2022-10-31 16:55:40 +0000

    checkin 3 digit numbers tru or pass

---
+++ main.py
@@ -0,0 +1,11 @@
+# from methods_help import method_help
+# from functions import function
+# from returnStuff import returnS
+from dynamic_functions import check_3Digits
+
+# method_help()
+# function()
+# returnS()
+# sum = 526 + 345
+result = check_3Digits([55,99, 6000])
+print(result)

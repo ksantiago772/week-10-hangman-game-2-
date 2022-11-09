148860dad5703d7bb2bcbb63973a5024ddc562cc
Kevin Santiago <113930456+ksantiago772@users.noreply.github.com>
2022-11-09 17:58:58 +0000

    mix sticks game

--- dynamic_functions.py
+++ dynamic_functions.py
@@ -15,12 +15,21 @@ def check_3Digits(list1):
 # Create a function (all_positives) that returns True if all the values in a list are positive, and False if at least one of the values is negative. Create a list named numbers with positive and negative values.
 
 # Don't call the function, you just need to define it.
-
+def all_positives(list1):
+   for n in list1:
+     if n > 0:
+      print (" your number is positive")
+     else:
+      print("your number is negative")
+   
 
 ########################################################################################################################
 # Dynamic Functions Practice #2
 # Create a function (sum_less) that adds the numbers of a list (stored in the variable numbers) as long as they are greater than 0 and less than 1000, and returns the result of said sum.
-
+def sum_less(list1):
+  for n in list1: 
+    if n > 0 and n<100:
+      
 
 
 ########################################################################################################################
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # from methods_help import method_help
 # from functions import function
 # from returnStuff import returnS
-from dynamic_functions import check_3Digits
-
+# from dynamic_functions import check_3Digits, all_positives
+from function_interactions import mix_sticks
+sticks = ["-", "--", "---", "----", "-----"]
+my_mix = mix_sticks(sticks)
+print(my_mix)
 # method_help()
 # function()
 # returnS()
 # sum = 526 + 345
-result = check_3Digits([55,99, 600])
-print(result)
+# result = check_3Digits([55,99, 600])
+# print(result)
+# all_positives([50])
